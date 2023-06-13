3ad5de1c5fc9f181ad9afcdbc77d543e6b09ceba
Azahramirez <88936883+Azahramirez@users.noreply.github.com>
2023-06-12 23:25:42 -0600

    Agregar excel e imagen

--- ejemplo.py
+++ ejemplo.py
@@ -7,29 +7,15 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.write("<h1 style='text-align: center;'>Centered Text</h1>", unsafe_allow_html=True)
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+st.write("<h1 style='text-align: center;'>Stream IA project</h1>", unsafe_allow_html=True)
+url = "W2/imagen.png"
+st.image(url, caption='Logo del equipo')
+st.write("<h2 style='text-align: left;'>Ranking mejores OSF</h2>", unsafe_allow_html=True)
+dataframe = pd.read_excel("W2/RankingFranco.xlsx")
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
 
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(dataframe)
+st.dataframe(dataframe)
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
